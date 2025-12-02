7e67537e1726a164f48edb3a1c027d8e5c2f2b68
Junjiecheng2024 <c15919822199@gmail.com>
2025-12-02 15:00:17 +0000

    update

---
+++ VecSetX/run_phase1_test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+module load python-data/3.10-24.04
+source /projappl/project_2016517/JunjieCheng/junjieenv/bin/activate
+
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+export OMP_NUM_THREADS=8
+
+
+# Create logs directory
+mkdir -p logs
+
+# Run training with torchrun
+cd /projappl/project_2016517/JunjieCheng/VecSetX
+
+torchrun --nproc_per_node=1 --master_port=29500 VecSetX/vecset/main_ae.py \
+    --batch_size 64 \
+    --model learnable_vec1024x16_dim1024_depth24_nb \
+    --point_cloud_size 8192 \
+    --input_dim 13 \
+    --epochs 800 \
+    --data_path /scratch/project_2016517/junjie/dataset/repaired_npz \
+    --output_dir output/ae/phase1_production \
+    --log_dir output/ae/phase1_production \
+    --wandb
