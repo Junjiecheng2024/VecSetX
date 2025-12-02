ad8543507bfb4688dccb4dfabad9012f672bff9f
Junjiecheng2024 <c15919822199@gmail.com>
2025-12-02 17:10:31 +0000

    update

--- VecSetX/run_phase1_test.sh
+++ VecSetX/run_phase1_test.sh
@@ -11,12 +11,14 @@ export OMP_NUM_THREADS=8
 # Create logs directory
 mkdir -p logs
 
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
 # Run training with torchrun
 cd /projappl/project_2016517/JunjieCheng/VecSetX
 
 torchrun --nproc_per_node=1 --master_port=29500 VecSetX/vecset/main_ae.py \
-    --batch_size 8 \
-    --accum_iter 8 \
+    --batch_size 2 \
+    --accum_iter 32 \
     --model learnable_vec1024x16_dim1024_depth24_nb \
     --point_cloud_size 8192 \
     --input_dim 13 \
