6445423635bd6d93513e5d473d3aa69a61e45475
Junjiecheng2024 <c15919822199@gmail.com>
2025-12-02 16:19:44 +0000

    update

--- VecSetX/vecset/models/utils.py
+++ VecSetX/vecset/models/utils.py
@@ -140,22 +140,6 @@ class Attention(nn.Module):
         h = self.heads
 
         q = self.to_q(x)
-        context = default(context, x)
-        kv = self.to_kv(context)
-
-        q = rearrange(q, 'b n (h d) -> b n h d', h = h)
-        kv = rearrange(kv, 'b n (p h d) -> b n p h d', h = h, p=2)
-
-        out = flash_attn_kvpacked_func(q.bfloat16(), kv.bfloat16(), window_size=(window_size, window_size))
-        out = out.to(x.dtype)
-
-        return self.to_out(rearrange(out, 'b n h d -> b n (h d)'))
-        
-class PointEmbed(nn.Module):
-    def __init__(self, hidden_dim=48, dim=128, input_dim=3):
-        super().__init__()
-
-        assert hidden_dim % 6 == 0
 
         self.embedding_dim = hidden_dim
         e = torch.pow(2, torch.arange(self.embedding_dim // 6)).float() * np.pi
