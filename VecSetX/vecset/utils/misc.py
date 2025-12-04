e6db25b87693f5899e15d7e3dcb600c1a0978e9e
Junjiecheng2024 <c15919822199@gmail.com>
2025-12-04 06:03:32 +0000

    update

--- VecSetX/vecset/utils/misc.py
+++ VecSetX/vecset/utils/misc.py
@@ -1,3 +1,5 @@
+# Copyright (c) 2025, Biao Zhang.
+
 import builtins
 import datetime
 import os
@@ -7,8 +9,7 @@ from pathlib import Path
 
 import torch
 import torch.distributed as dist
-from torch import inf
-
+from torch._six import inf
 
 class SmoothedValue(object):
     """Track a series of values and provide access to smoothed values over a
@@ -276,22 +277,32 @@ def get_grad_norm_(parameters, norm_type: float = 2.0) -> torch.Tensor:
     device = parameters[0].grad.device
     if norm_type == inf:
         total_norm = max(p.grad.detach().abs().max().to(device) for p in parameters)
+    else:
+        total_norm = torch.norm(torch.stack([torch.norm(p.grad.detach(), norm_type).to(device) for p in parameters]), norm_type)
+    return total_norm
+
+
+def save_model(args, epoch, model, model_without_ddp, optimizer, loss_scaler, tag=None):
+    output_dir = Path(args.output_dir)
+    epoch_name = str(epoch)
+    if tag is not None:
+        epoch_name = tag
+        
+    if loss_scaler is not None:
         checkpoint_paths = [output_dir / ('checkpoint-%s.pth' % epoch_name)]
         for checkpoint_path in checkpoint_paths:
-            for i, model_without_ddp in enumerate(models_without_ddp):
-                to_save = {
-                    'model': model_without_ddp.state_dict(),
-                    'optimizer': optimizer.state_dict(),
-                    'epoch': epoch,
-                    'scaler': loss_scaler.state_dict(),
-                    'args': args,
-                }
+            to_save = {
+                'model': model_without_ddp.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'epoch': epoch,
+                'scaler': loss_scaler.state_dict(),
+                'args': args,
+            }
 
-                save_on_master(to_save, checkpoint_path+'%02d'.format(i))
+            save_on_master(to_save, checkpoint_path)
     else:
         client_state = {'epoch': epoch}
-        models.save_checkpoint(save_dir=args.output_dir, tag="checkpoint-%s" % epoch_name, client_state=client_state)
-
+        model.save_checkpoint(save_dir=args.output_dir, tag="checkpoint-%s" % epoch_name, client_state=client_state)
 
 def load_model(args, model_without_ddp, optimizer, loss_scaler):
     if args.resume:
